9420975618bb482cd4c8b97c6fec0448130a1469
manishks040 <125584575+manishks040@users.noreply.github.com>
2023-07-04 22:04:29 +0530

    Update main.cpp (final commit)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+//      Date: Tue 4 Jul 10:20 PM
+// Developer: Manish Kumar Singh
+// This code can be used for educational purpose only, not for sale. If 
+// anyone found misusing it then he/she will be sued in court!
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
-// For Windows:
-    // #include<windows.h>
+#include<windows.h>
+
 using std::cout;
 using std::cin;
 using std::endl;
@@ -25,11 +29,11 @@ int main(){
 
     for(int i = time; i> 0 ; i--){
         system("clear");
-        // system("cls"); // for Windows
+        system("cls"); // for Windows
         cout<< "The time remaining is 0:" << minutes << ":" << seconds << endl;
         minutes = i % 60 == 0 ? --minutes : minutes;
         seconds = seconds == 0 ? 59 : --seconds;
-        // system("sleep 1");
+        system("sleep 1");
         sleep(1); // the same effect.
         // system("TIMEOUT 1"); // kinda
     }
